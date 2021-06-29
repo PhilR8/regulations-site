bee3f056776c7adfa22198462994c6889cc87646
Alex Bradley <alex.robert.bradley@gmail.com>
2021-06-29 10:31:28 -0400

    placed by_effective_on in its own function

--- regulations/static/regulations/js/RelatedRules.js
+++ regulations/static/regulations/js/RelatedRules.js
@@ -317,10 +317,10 @@ var script = {
         async fetch_rules(title, part) {
             const response = await fetch(`https://www.federalregister.gov/api/v1/documents.json?fields[]=type&fields[]=abstract&fields[]=citation&fields[]=correction_of&fields[]=dates&fields[]=docket_id&fields[]=docket_ids&fields[]=document_number&fields[]=effective_on&fields[]=html_url&fields[]=publication_date&fields[]=regulation_id_number_info&fields[]=regulation_id_numbers&fields[]=title&per_page=20&order=newest&conditions[type][]=RULE&conditions[cfr][title]=${title}&conditions[cfr][part]=${part}`);
             const rules = await response.json();
-            const results = rules.results.sort((a, b) => {
+            const by_effective_on = (a,b) => {
               return new Date(b.effective_on) - new Date(a.effective_on);
-            });
-            return results;
+            };
+            return rules.results.sort(by_effective_on);
         }
     }
 };
